e7211efd432cd7a389100cc13344a292f29065be
Mehdi <mehditeymorian322@gmail.com>
2019-05-06 18:07:49 +0430

    Add LibraryCreation Test

---
+++ src/Tests/LibraryCreation.java
@@ -0,0 +1,12 @@
+import org.RectSky.NumberMatrix;
+import org.junit.Test;
+
+public class LibraryCreation {
+
+
+    @Test
+    public void createNumberMatrix(){
+        NumberMatrix numberMatrix = new NumberMatrix(1,2);
+
+    }
+}
